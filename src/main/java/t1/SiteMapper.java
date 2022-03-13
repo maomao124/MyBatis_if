eddfb852768fb233c13e6b7fec1cf123217d2852
毛 <1296193245@qq.com>
2022-03-13 21:02:11 +0800

    MyBatis_if

---
+++ src/main/java/t1/SiteMapper.java
@@ -0,0 +1,26 @@
+package t1;
+
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Date;
+
+/**
+ * Project name(项目名称)：MyBatis_if
+ * Package(包名): t1
+ * Interface(接口名): SiteMapper
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/3/13
+ * Time(创建时间)： 20:20
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public interface SiteMapper
+{
+    public int update(@Param("site") Site site);
+
+    public int updateByParam(@Param("id") Integer id, @Param("name") String name, @Param("url") String url,
+                             @Param("age") Integer age, @Param("country") String country, @Param("createTime") Date createTime);
+}
---
+++ src/test/java/t1/SiteMapperTest.java
@@ -0,0 +1,76 @@
+package t1;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Project name(项目名称)：MyBatis_if
+ * Package(包名): t1
+ * Class(测试类名): SiteMapperTest
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/3/13
+ * Time(创建时间)： 20:39
+ * Version(版本): 1.0
+ * Description(描述)： 测试类
+ */
+
+class SiteMapperTest
+{
+
+    @Test
+    void update() throws IOException
+    {
+        //读取配置文件mybatis-config.xml
+        InputStream config = Resources.getResourceAsStream("mybatis-config.xml");
+        //根据配置文件构建SqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(config);
+        //通过SqlSessionFactory创建SqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        Site site = new Site();
+        site.setId(7);
+        site.setAge(13);
+        site.setCountry("cn");
+
+        SiteMapper siteMapper = sqlSession.getMapper(SiteMapper.class);
+        int i = siteMapper.update(site);
+        System.out.println(i);
+
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    void updateByParam() throws IOException
+    {
+        //读取配置文件mybatis-config.xml
+        InputStream config = Resources.getResourceAsStream("mybatis-config.xml");
+        //根据配置文件构建SqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(config);
+        //通过SqlSessionFactory创建SqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+//        Site site = new Site();
+//        site.setId(7);
+//        site.setAge(13);
+//        site.setCountry("cn");
+
+        SiteMapper siteMapper = sqlSession.getMapper(SiteMapper.class);
+        int i = siteMapper.updateByParam(7, null, null, 12, null, new Date());
+        System.out.println(i);
+
+        sqlSession.commit();
+        sqlSession.close();
+    }
+}
